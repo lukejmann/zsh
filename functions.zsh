542a4c8d15ae76a4329aca662600015205f1d9c4
Luke Mann <luke@Lukes-MacBook-Pro.local>
2020-07-30 20:07:02 -0400

    go

--- functions.zsh
+++ functions.zsh
@@ -3,3 +3,14 @@ function lgit() {
     git commit -a -m "$1"
     git push
 }
+
+function mkg() {
+    mkdir $1
+    cd $1
+}
+
+function update-go(){
+    git clone https://github.com/udhos/update-golang
+    cd update-golang
+    sudo ./update-golang.sh
+}
